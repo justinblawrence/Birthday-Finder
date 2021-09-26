2c677a85d37ac0166b3356092265da005b4cb9df
Justin Lawrence <88911881+justinblawrence@users.noreply.github.com>
2021-09-25 17:18:28 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -24,7 +24,6 @@ public class Main implements blueprint {
         }
         for(int i = 0 ; i < list.size()-1; i++){
             map.put(list.get(i), list.get(i+1));
-
         }
         return map;
     }
